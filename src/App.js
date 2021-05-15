b73ba44d82a03569337f95b9b6a8580ee472e1f9
orlandollerena <trancetribal@bellsouth.net>
2021-05-14 22:53:44 -0400

    fix file name errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import Sidebar from "../src/Sidebar/Sidebar";
 
 function App() {
--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,10 +1,15 @@
 import React from "react";
-import "./sidebar.sass";
+import "./sidebar.scss";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import SidebarOptions from "../SidebarOptions/SidebarOptions";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Sidebar() {
   return (
@@ -15,10 +20,11 @@ function Sidebar() {
       <SidebarOptions Icon={HomeIcon} text="Home" />
       <SidebarOptions Icon={SearchIcon} text="Explore" />
       <SidebarOptions Icon={NotificationsNoneIcon} text="Notifications" />
-
-      {/* sidebar options */}
-      {/* sidebar options */}
-      {/* sidebar options */}
+      <SidebarOptions Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOptions Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOptions Icon={ListAltIcon} text="Lists" />
+      <SidebarOptions Icon={PermIdentityIcon} text="Profiles" />
+      <SidebarOptions Icon={MoreHorizIcon} text="More" />
 
       {/* Button -> tweet */}
     </div>
