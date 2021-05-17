7e65faf8103860a84c9561ed83b345b500336d0a
orlandollerena <trancetribal@bellsouth.net>
2021-05-16 21:42:24 -0400

    working on firebase

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDnvQeNfWennlGbRb6Frb0mUVgatBBshpY",
+  authDomain: "twitter-clone-practice.firebaseapp.com",
+  projectId: "twitter-clone-practice",
+  storageBucket: "twitter-clone-practice.appspot.com",
+  messagingSenderId: "1053345237778",
+  appId: "1:1053345237778:web:cdf4b8b728e3bdaed581cb",
+  measurementId: "G-JDZGL387LH",
+};
