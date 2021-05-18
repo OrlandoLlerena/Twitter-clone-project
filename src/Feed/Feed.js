95b5d1eb1521a266eafaf3fa5ad2470a7be80bd9
orlandollerena <trancetribal@bellsouth.net>
2021-05-17 21:26:37 -0400

    firebase deploy

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -3,16 +3,15 @@ import "./feed.scss";
 import TweetBox from "../TweetBox/Tweetbox";
 import Post from "../Post/Post";
 import db from "../Firebase";
+import Flipmove from "react-flip-move";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
-
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) =>
       setPosts(snapshot.docs.map((doc) => doc.data()))
     );
   }, []);
-
   return (
     <div className="feed">
       <div className="feed__header">
@@ -20,25 +19,27 @@ function Feed() {
       </div>
       <TweetBox />
       <div>
-        {posts.map((post) => (
-          <Post
-            displayName={post.displayName}
-            username={post.username}
-            verified={post.verified}
-            text={post.text}
-            avatar={post.avatar}
-            image={post.image}
-          />
-        ))}
-
-        <Post
+        <Flipmove>
+          {posts.map((post) => (
+            <Post
+              key={post.text}
+              displayName={post.displayName}
+              username={post.username}
+              verified={post.verified}
+              text={post.text}
+              avatar={post.avatar}
+              image={post.image}
+            />
+          ))}
+        </Flipmove>
+        {/* <Post
           displayName="Orlando Llerena"
           username="hulksmash"
           verified={true}
           text="Easy like Sunday Morning"
           avatar="https://pbs.twimg.com/profile_images/1222657003350319106/f7QwpKjF_400x400.jpg"
           image="https://media.giphy.com/media/PmGJCIL6mImz0LHoAH/giphy.gif"
-        />
+        /> */}
       </div>
     </div>
   );
